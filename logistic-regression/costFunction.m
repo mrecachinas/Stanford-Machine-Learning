a1448cd2e888394de4a48c266b7eae989f96d511
Michael Recachinas <mgr3yp@virginia.edu>
2014-07-08 20:46:37 -0400

    Finished sigmoid, cost, and gradient descent

--- logistic-regression/costFunction.m
+++ logistic-regression/costFunction.m
@@ -20,14 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
 coeff = 1 / m;
 h = sigmoid(theta' * X');
 J = coeff .* sum(-y' .* log(h) - (1 - y') .* log(1 - h));
 grad = coeff * (h - y') * X;
-
-
-
 
 % =============================================================
 
