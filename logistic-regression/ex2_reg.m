e022c18d225b7318911934d5aec595a09dd6c4b4
Michael Recachinas <mgr3yp@virginia.edu>
2014-07-12 14:22:32 -0400

    Tried various regularization params

--- logistic-regression/ex2_reg.m
+++ logistic-regression/ex2_reg.m
@@ -87,7 +87,7 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 1;
+lambda = 1; % Try 0, 1, 100
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
